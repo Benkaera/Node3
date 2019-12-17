19d86a950aa9891d9e1d0dca2abc46d4abaae245
Benkaera <50444630+Benkaera@users.noreply.github.com>
2019-12-17 22:58:21 +0300

    27

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const express = require("express");
-const bodyParser = require("body-parser");
- const app = express();
-  // создаем парсер для данных application/x-www-form-urlencoded
-const urlencodedParser = bodyParser.urlencoded({extended: false});
- app.get("/register", urlencodedParser, function (request, response) {
-    response.sendFile(__dirname + "/register.html");
-});
-app.post("/register", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
-    console.log(request.body);
-    response.send(`${request.body.userName} - ${request.body.userAge}`);
-});
-  app.get("/", function(request, response){
-    response.send("Главная страница");
-});
+const app = express();
+app.get("/categories/:categoryId/products/:productId", function (request, response) {
+        let catId = request.params["categoryId"];
+        let prodId = request.params["productId"];
+        response.send(`Категория: ${catId}  Товар: ${prodId}`);
+    });
+    
+ app.listen(3000);
+
+    
