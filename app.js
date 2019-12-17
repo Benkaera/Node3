d61dd665592631d16354f49a69403d13b4a58f27
Benkaera <50444630+Benkaera@users.noreply.github.com>
2019-12-17 23:14:42 +0300

    32

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const app = express();
-app.get("/categories/:categoryId/products/:productId", function (request, response) {
-        let catId = request.params["categoryId"];
-        let prodId = request.params["productId"];
-        response.send(`Категория: ${catId}  Товар: ${prodId}`);
-    });
-    
- app.listen(3000);
-
-    
+  const app = express();
+// создаем парсер для данных в формате json
+const jsonParser = express.json();
+  app.post("/user", jsonParser, function (request, response) {
+    console.log(request.body);
+    if(!request.body) return response.sendStatus(400);
+         response.json(request.body); // отправляем пришедший ответ обратно
+});
+  app.get("/", function(request, response){
+          response.sendFile(__dirname + "/index.html");
+});
+  app.listen(3000);
